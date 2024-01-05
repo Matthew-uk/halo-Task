66d3b9e9f474a9c0781aba9e225f2e8262b872b9
Matthew-uk <esaduviedede@gmail.com>
2024-01-05 08:23:10 +0100

    Latest push on Balablu

---
+++ app/balablu/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Balablue = () => {
+  return <div>A town Hall Different Balalblu</div>;
+};
+
+export default Balablue;
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
@@ -5,9 +6,14 @@ const Navbar = () => {
     <div className="md:px-20 px-5 py-5 flex flex-row w-full justify-between">
       <div className="logo text-2xl font-bold">Halo-Task</div>
       <div className="buttons">
-        <button className="border-2 text-orange-text border-orange-text rounded-full px-4 py-1">
+        <Link href="/login">
+          <button className="border-2 text-orange-text border-orange-text rounded-full px-4 py-1 mr-4">
+            Login
+          </button>
+        </Link>
+        {/* <button className="border-2 text-orange-text border-orange-text rounded-full px-4 py-1">
           Login
-        </button>
+        </button> */}
       </div>
     </div>
   );
