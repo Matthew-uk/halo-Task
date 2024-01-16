a275c182fa1e3013d3d58a0ccc796206aa1ce2df
Matthew-uk <esaduviedede@gmail.com>
2024-01-16 15:03:50 +0100

    Adding Proper Loading...

--- app/dashboard/[...id]/page.tsx
+++ app/dashboard/[...id]/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import DashboardLayout from "@/app/components/dashboardLayout";
-import useUserStore from "@/store/store";
+import React, { CSSProperties, useEffect, useState } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 import Link from "next/link";
-import React, { CSSProperties, useEffect, useState } from "react";
 import { BeatLoader } from "react-spinners";
+import DashboardLayout from "@/app/components/dashboardLayout";
+import useUserStore from "@/store/store";
 
 const override: CSSProperties = {
   display: "block",
@@ -20,9 +20,10 @@ const Page = ({ params }: any) => {
   useEffect(() => {
     const fetchData = async () => {
       const token = Cookies.get("user");
+
       try {
         setLoading(true);
-        const res = await axios.get(
+        const response = await axios.get(
           "https://halo-task-backend.onrender.com/api/v1/user",
           {
             headers: {
@@ -30,14 +31,17 @@ const Page = ({ params }: any) => {
             },
           }
         );
-        setUserName(`${res.data.username}`);
-        console.log(res.data.username);
+
+        const username = response.data.username;
+        setUserName(username);
+        console.log(username);
       } catch (error: any) {
-        console.log(error.message);
+        console.error("Error fetching user data:", error.message);
       } finally {
         setLoading(false);
       }
     };
+
     fetchData();
   }, []);
 
@@ -60,12 +64,10 @@ const Page = ({ params }: any) => {
             {/* Header */}
             <div className="w-full flex justify-between items-center">
               <h2 className="">You have 0 Tasks</h2>
-              <Link
-                href={"/dashboard/addtask"}
-                className="px-6 py-2 bg-gray-text text-white rounded-md"
-                onClick={() => console.log(params)}
-              >
-                + Task
+              <Link href="/dashboard/addtask">
+                <a className="px-6 py-2 bg-gray-text text-white rounded-md">
+                  + Task
+                </a>
               </Link>
             </div>
             <div>
