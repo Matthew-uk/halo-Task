a975b4bf794bcaa90d56214b033f6bd88cb6147c
Matthew-uk <esaduviedede@gmail.com>
2024-01-30 11:27:08 +0100

    Fixing some minor things on the Toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
           pauseOnFocusLoss
           draggable
           pauseOnHover
-          theme="colored"
+          // theme="colored"
         />
         {children}
       </body>
